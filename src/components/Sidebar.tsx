cb6c35a2d9d1a36b186a7e87dd354701d93e520c
Marcin Libelt <marcin.libelt@growcode.com>
2023-11-02 17:32:00 +0100

    components with no logic 2

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import JobList from "./JobList";
+import Pagination from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingControls from "./SortingControls";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <ResultsCount />
+        <SortingControls />
+      </div>
+      <JobList />
+      <Pagination />
+    </div>
+  );
+}
